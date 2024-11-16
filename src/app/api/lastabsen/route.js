3f2360ea34f0ef7daaeaa37ff3ab6f6aa2b1d9f8
tmmin <tmminunion@gmail.com>
2024-11-16 20:03:42 +0700

    new ALl Data2

---
+++ src/app/api/lastabsen/route.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export async function GET() {
+  try {
+    // Panggil API eksternal
+    const response = await axios.get(
+      "https://api.bungtemin.net/FamgetAbsensi/lastabsen"
+    );
+
+    // Ambil data dari response
+    const data = response.data;
+
+    // Kirimkan data sebagai response
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch {
+    return new Response(
+      JSON.stringify({ message: "Token dan topik harus disediakan" }),
+      { status: 400 }
+    );
+  }
+}
